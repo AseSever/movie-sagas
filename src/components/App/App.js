86ede1f5ff6773d105caa0ba65a34567b299dc00
Alex Severson <alexsvrsn@gmail.com>
2020-08-28 16:23:32 -0500

    added Home component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Home from '../Home/Home'
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <h1>Movies!</h1>
         <Router>
+          <Route exact path="/" component={Home} />
           {/* ADD PAGES! */}
         </Router>
         <p>Empty Page</p>
